1c112a8dfcc75d4dae91c76d4fd5d3d574fde7c2
tiago maffei <tiagomaffei@gmail.com>
2019-04-04 14:45:40 -0600

    all but black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
-
+import Post from "./Post/Post"
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Search from './Header/Search/Search';
 
 class App extends Component {
   constructor() {
@@ -19,19 +20,37 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then( res => {
+      console.log(res)  
+    this.setState({posts:res.data})
+    })
 
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then( res => {
+      this.setState({posts:res.data})
+    })
   }
 
-  deletePost() {
+  searchPost(text) {
+    axios.get(`https://practiceapi.devmountain.com/api/posts?text=${text}`).then( res => {
+      console.log(res)  
+    this.setState({posts:res.data})
+    })
 
   }
 
-  createPost() {
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then( res => {
+      this.setState({posts:res.data})
+    }) 
+  }
 
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts',{text}).then( res => {
+      this.setState({posts:res.data})
+    }) 
   }
 
   render() {
@@ -43,7 +62,21 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose 
+          createPostFn={this.createPost}/>
+          <Search
+          searchPostFn={this.searchPost} />
+          {
+            posts.map( post => (
+              <Post key={post.id} 
+                  text={post.text} 
+                  date={post.date}
+                  id={post.id}
+                  updatePostFn={this.updatePost}
+                  deletePostFn={this.deletePost}
+                  />
+            ))
+          }
           
         </section>
       </div>
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,6 +7,25 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      text: ''
+    };
+
+    this.searchPost = this.searchPost.bind( this );
+  }
+  
+  
+  searchPost() {
+    const {text} = this.state
+    const {searchPostFn} = this.props
+    searchPostFn(text);
+    //this.setState({text :""})
+    console.log(text)
+  }
+  
   render() {
     return (
       <section className="Search__parent">
